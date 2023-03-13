5170044642840e1b961e12a1ab29286dbe4e0296
Patrick <patrickisiah2001@gmail.com>
2023-03-12 18:28:57 -0700

    Fixed tomatoes for Fred Meyers.

--- database_functions.py
+++ database_functions.py
@@ -1,3 +1,8 @@
+"""
+Functions responsible for retrieving and updating prices of items
+to/from the MySQL database to be displayed onto the user interface.
+Author: Patrick Rodriguez
+"""
 import mysql.connector
 from datetime import datetime
 
@@ -7,6 +12,11 @@ from Table_Class import Table
 
 
 def clear_date_table():
+    """
+    Clears the MySQL last_updated table.
+    Returns: N/A
+    """
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -16,12 +26,18 @@ def clear_date_table():
     )
 
     cursor = con.cursor()
-    cursor.execute("DELETE FROM last_updated")
-    con.commit()
-    con.close()
+    cursor.execute("DELETE FROM last_updated")  # MySQL query responsible for clearing table.
+    con.commit()  # Commit MySQL query.
+    con.close()  # Close MySQL connection.
 
 
 def update_last_updated():
+    """
+    Modifies the last_updated entry table responsible for indicating
+    when the update() function was last ran by a user.
+    Returns: N/A
+    """
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -31,15 +47,19 @@ def update_last_updated():
     )
 
     cursor = con.cursor()
-    unprocessed_date = datetime.today()
-    dt_string = unprocessed_date.strftime("%m/%d/%Y %H:%M:%S")
+    unprocessed_date = datetime.today()  # Grab today's date.
+    dt_string = unprocessed_date.strftime("%m/%d/%Y %H:%M:%S")  # String processing today's date.
 
-    cursor.execute(f"insert into last_updated values ('{dt_string}')")
-    con.commit()
-    con.close()
+    cursor.execute(f"insert into last_updated values ('{dt_string}')")  # MySQL query to update the entry.
+    con.commit()  # Commit changes to table.
+    con.close()  # Close MySQL connection.
 
 
 def get_last_updated():
+    """
+    Returns the date and time when the MySQL database was last updated.
+    """
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -49,15 +69,17 @@ def get_last_updated():
     )
 
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM last_updated")
-    return cursor.fetchall()[0][0]
+    cursor.execute("SELECT * FROM last_updated")  # MySQL query responsible for gathering date.
+    return cursor.fetchall()[0][0]  # Returns date and time string.
 
 
 def clear_fredmeyers_table():
     """
     Clears all data in fredmeyers MySQL database table.
     To be used when user updates prices.
+    Returns: N/A
     """
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -67,18 +89,19 @@ def clear_fredmeyers_table():
     )
 
     cursor = con.cursor()
-    cursor.execute("DELETE FROM fredmeyers")
-    con.commit()
-    con.close()
+    cursor.execute("DELETE FROM fredmeyers")  # MySQL query to clear table.
+    con.commit()  # Commit changes to MySQL.
+    con.close()  # Close connection to MySQL database.
 
 
 def insert_to_fredmeyers_table(item: str, price: float):
     """
     Inserts an item-price combination into the Fred Meyers MySQL database table.
-    To be used when mass-updating the database table.
+    To be used when mass-updating the database table from scrapers.
+    Returns: N/A
     """
 
-    # MySQL connection information
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -89,11 +112,10 @@ def insert_to_fredmeyers_table(item: str, price: float):
 
     cursor = con.cursor()
 
-    # MySQL query that inserts values into database.
-    query = f"insert into fredmeyers values('{item}', {price})"
+    query = f"insert into fredmeyers values('{item}', {price})"  # MySQL query that inserts values into database.
     cursor.execute(query)
-    con.commit()
-    con.close()
+    con.commit()  # Commit changes to MySQL database
+    con.close()  # Close connection to MySQL
 
 
 def mass_insert_fredmeyers_table(items: list):
@@ -103,15 +125,12 @@ def mass_insert_fredmeyers_table(items: list):
     into the MySQL database table.
     """
     for item_price_tuple in items:
-        item = item_price_tuple[0].replace(r"'", r"\'")
+        item = item_price_tuple[0].replace(r"'", r"\'")  # Addresses edge case of any apostrophes in item_name.
         price = item_price_tuple[1]
         insert_to_fredmeyers_table(item, price)
 
-
+"""
 def update_fredmeyers_table(item: str, price: float):
-    """
-    To be called by the user to adjust any incorrect prices.
-    """
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -128,7 +147,7 @@ def update_fredmeyers_table(item: str, price: float):
     cursor.execute(query)
     con.commit()
     con.close()
-
+"""
 
 def get_fredmeyers_prices(item: str):
     """
@@ -136,7 +155,7 @@ def get_fredmeyers_prices(item: str):
     displayed to the user.
     """
 
-    # Edge cases in order to obtain the correct MySQL entries.
+    # Addressing edge cases in order to obtain the correct MySQL entries.
     if item == "Milk (1gal)":
         item = "Milk"
     if item == "Butter":
@@ -155,7 +174,10 @@ def get_fredmeyers_prices(item: str):
         item = "Flour"
     if item == "Salt":
         item = "Iodized Salt"
+    if item == "Tomatoes":
+        item = "Roma Tomato"
 
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -166,13 +188,14 @@ def get_fredmeyers_prices(item: str):
 
     cursor = con.cursor()
 
+    # MySQL query responsible for obtaining appropriate information
     query = (f"SELECT *\n"
              f"FROM fredmeyers\n"
              f"WHERE item_name LIKE '%{item}%'")
 
     cursor.execute(query)
-    data = cursor.fetchall()
-    con.close()
+    data = cursor.fetchall()  # Variable that contains appropriate data entries.
+    con.close()  # Close connection to MySQL database.
 
     return data[0][1]
 
@@ -182,6 +205,7 @@ def clear_target_table():
     Clears all data in target MySQL database table.
     To be used when user updates prices.
     """
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -191,9 +215,9 @@ def clear_target_table():
     )
 
     cursor = con.cursor()
-    cursor.execute("DELETE FROM target")
-    con.commit()
-    con.close()
+    cursor.execute("DELETE FROM target")  # MySQL query to clear target table.
+    con.commit()  # Commit changes to MySQL database.
+    con.close()  # Close connection to MySQL database.
 
 
 def insert_to_target_table(item: str, price: float):
@@ -201,10 +225,11 @@ def insert_to_target_table(item: str, price: float):
     Inserts an item-price combination into the Fred Meyers MySQL database table.
     To be used when mass-updating the database table.
     """
+    # Edge case to avoid getting "tomato" and "tomato ketchup" when gathering price of tomato.
     if item == "Roma Tomato":
         item = "Roma Tomatoes"
 
-    # MySQL connection information
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -215,11 +240,10 @@ def insert_to_target_table(item: str, price: float):
 
     cursor = con.cursor()
 
-    # MySQL query that inserts values into database.
-    query = f"insert into target values('{item}', {price})"
+    query = f"insert into target values('{item}', {price})" # MySQL query that inserts values into database.
     cursor.execute(query)
-    con.commit()
-    con.close()
+    con.commit()  # Commit changes to MySQL database
+    con.close()  # Close connection to MySQL database.
 
 
 def mass_insert_target_table(items: list):
@@ -229,15 +253,12 @@ def mass_insert_target_table(items: list):
     into the MySQL database table.
     """
     for item_price_tuple in items:
-        item = item_price_tuple[0].replace(r"'", r"\'")
+        item = item_price_tuple[0].replace(r"'", r"\'") # Addresses edge case of any apostrophes in item_name.
         price = item_price_tuple[1]
-        insert_to_target_table(item, price)
+        insert_to_target_table(item, price)  # Insert entry to MySQL database.
 
-
+"""
 def update_target_table(item: str, price: float):
-    """
-    To be called by the user to adjust any incorrect prices.
-    """
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -254,7 +275,7 @@ def update_target_table(item: str, price: float):
     cursor.execute(query)
     con.commit()
     con.close()
-
+"""
 
 def get_target_prices(item: str):
     """
@@ -279,6 +300,7 @@ def get_target_prices(item: str):
     if item == "Salt":
         item = "Iodized Salt"
 
+    # Information needed to connect to MySQL database.
     con = mysql.connector.connect(
         host='ix-dev.cs.uoregon.edu',
         port=3820,
@@ -289,40 +311,59 @@ def get_target_prices(item: str):
 
     cursor = con.cursor()
 
+    # MySQL query to obtain necessary data entries.
     query = (f"SELECT *\n"
              f"FROM target\n"
              f"WHERE item_name LIKE '%{item}%'")
 
+    # Edge case with butter vs peanut butter.
     if item == "Butter":
         query = (f"SELECT *\n"
                  f"FROM target\n"
                  f"WHERE item_name = 'Butter'")
 
     cursor.execute(query)
-    data = cursor.fetchall()
-    con.close()
+    data = cursor.fetchall()  # Variable contains all necessary data entries.
+    con.close()  # Close connection to MySQL database.
 
     return data
 
 
 def update_prices():
+    """
+    The function to be called by the update button. It will clear and repopulate MySQL tables
+    by running the scraper and calling functions to insert data into necessary tables.
+    """
+
+    # Clear MySQL tables
     clear_target_table()
     clear_fredmeyers_table()
     clear_date_table()
 
+    # Gathers updated prices by calling all scrapers.
     target_data = Target_Scraper.Target_Scrapper()
     fredmeyer_data = FredMeyer_Scraper.scrape()
 
+    # Insert the updated prices into the MySQL table.
     mass_insert_target_table(target_data)
     mass_insert_fredmeyers_table(fredmeyer_data)
     update_last_updated()
 
 
 def format_and_display(items: list[str]):
+    """
+    Takes in a list of staples. First, it will run appropriate functions to retrieve
+    appropriate prices from each grocery store. Then it will display and format the items and price
+    combinations via a Table class object
+    """
+    # data list used to initialize a Table class object
     data = [("", "Fred Meyer", "Target")]
     for item in items:
+        # grab price of item from each grocery store.
         fredmeyers_price = get_fredmeyers_prices(item)
         target_price = get_target_prices(item)
+
+        #format prices by (item, price#1, price#2) then append to data list.
         data.append((item, fredmeyers_price, target_price))
 
     return Table(data)
